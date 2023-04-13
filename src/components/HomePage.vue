85345f7150f75621c9570e7e684cfad33fe4c485
louisnoiz <63070163@kmitl.ac.th>
2023-04-13 16:14:51 +0700

    update: Homepage

---
+++ src/components/HomePage.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container is-widescreen">
+    <section class="hero">
+      <div class="hero-body">
+        <p class="title">
+          Your Trip
+        </p>
+        <div class="columns">
+          <div class="column is-half">
+            <input class="input" type="text" placeholder="Search Trip">
+          </div>
+          <div class="column is-half">
+            <button class="button">Search</button>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="content">
+        <div class="columns is-multiline">
+          <div class="column is-3">
+            <div class="card has-background-">
+              <div class="card-content">
+                <p class="title">
+                  “There are two hard things in computer science: cache invalidation, naming things, and off-by-one
+                  errors.”
+                </p>
+                <p class="subtitle">
+                  Jeff Atwood
+                </p>
+              </div>
+              <footer class="card-footer">
+                <p class="card-footer-item">
+                  <span>
+                    <a>Edit</a>
+                  </span>
+                </p>
+                <p class="card-footer-item">
+                  <span>
+                    <a>Delete</a>
+                  </span>
+                </p>
+              </footer>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
+import 'bulma/css/bulma.css'
 
 
 createApp(App).mount('#app')
