5a5c8d239e16dde81466c46bde069bfcad89f9c6
taifhoon <taifhoon432@gmail.com>
2023-04-14 21:58:07 +0700

    edit: Homepage

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -7,8 +7,7 @@
         </p>
         <div class="columns">
           <div class="column is-half">
-            <input class="input" type="text" placeholder="Search Trip">
-            
+            <input class="input" type="text" placeholder="Search Trip" />
           </div>
           <div class="column is-half">
             <button class="button">Search</button>
@@ -18,28 +17,20 @@
     </section>
     <section class="section">
       <div class="content">
-        <div class="columns is-multiline">
-          <div class="column is-5">
-            <div class="card has-background-link-light">
+        <div class="columns">
+          <div class="column">
+            <div class="card has-background-link-light mb-5" v-for="(trip, index) in listTrip" :key="index">
               <div class="card-content">
                 <p class="title">
-                  Songkran Trip
+                  {{ trip.name }}
                 </p>
-                <!-- <p class="subtitle">
-                  This is trip on 13 April 2023
-                </p> -->
+                <p class="subtitle">
+                  This is trip have duration {{ trip.day.length }} day
+                </p>
               </div>
               <footer class="card-footer">
-                <p class="card-footer-item">
-                  <span>
-                    <a>Edit</a>
-                  </span>
-                </p>
-                <p class="card-footer-item">
-                  <span>
-                    <a>Delete</a>
-                  </span>
-                </p>
+                <a class="card-footer-item" @click="edit()">Edit</a>
+                <a class="card-footer-item">Delete</a>
               </footer>
             </div>
           </div>
@@ -48,3 +39,42 @@
     </section>
   </div>
 </template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      listTrip: [
+        {
+          name: "Songkran Trip",
+          day: [
+            {
+              id: 1,
+              location: "aaa"
+            },
+            {
+              id: 2,
+              location: "bbb"
+            }
+          ]
+        },
+        {
+          name: "Japan",
+          day: [
+            {
+              id: 1,
+              location: "aaa"
+            }
+          ]
+        }
+      ]
+    }
+  },
+  methods: {
+    edit() {
+      this.$router.push({path: "/updatetrip"})
+    }
+  }
+}
+</script>
